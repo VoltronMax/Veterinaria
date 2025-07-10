a54a109c60adea21f78908fd9c7f1d26159eff34
Victor <vm869720@gmail.com>
2025-07-09 21:32:17 -0500

    Se empieza con el proyecto de veterinaria mediante clases

--- src/veterinaria/Dueño.java
+++ src/veterinaria/Dueño.java
@@ -3,12 +3,17 @@ package veterinaria;
 public class Dueño {
     private String nombre;
     private String apellido;
-    private String dni;
+    private String cedula;
     private String direccion;
     private String telefono;
 
     public String getNombre() {
         return nombre;
-
     }
+
+    public String getApellido(){
+        return apellido;
+    }
+
+
 }
